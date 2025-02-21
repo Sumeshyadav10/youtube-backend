514945f68802e1782a9c8b1f4469fabc51792802
Sumeshyadav10 <ysumesh084@gmail.com>
2025-02-21 23:55:49 +0530

    Added more controller

---
+++ src/controllers/changepassword.js
@@ -0,0 +1,77 @@
+const UserSchema = require('../models/User.model');
+const asynchandler = require('../utils/asynchandler');
+const apiError = require('../utils/apiError');
+const ApiResponse = require('../utils/apiResponse');
+
+const changePassword = asynchandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        throw new apiError(400, 'Please provide all the required details');
+    }
+
+    const user = await UserSchema.findById(req.user._id);
+
+    if (!user) {
+        throw new apiError(404, 'User not found');
+    }
+
+    const isPasswordMatch = await user.isPasswordMatch(oldPassword);
+
+    if (!isPasswordMatch) {
+        throw new apiError(400, 'Invalid credentials');
+    }
+
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(200).json(new ApiResponse(200, null, 'Password changed successfully'));
+});
+
+
+const getCurrentUser = asynchandler(async (req, res) => {
+    const user = await UserSchema.findById(req.user._id).select('-password -refresh_token').lean();
+    return res.status(200).json(new ApiResponse(200, user, 'User details'));
+}
+);
+
+const updateAccountDetails = asynchandler(async (req, res) => {
+    const { username, fullname, email } = req.body;
+
+    if (!username && !fullname && !email) {
+        throw new apiError(400, 'Please provide at least one field to update');
+    }
+
+    // const user = await UserSchema.findById(req.user._id);
+
+    // if (!user) {
+    //     throw new apiError(404, 'User not found');
+    // }
+
+    // if (username) {
+    //     user.username = username;
+    // }
+
+    // if (fullname) {
+    //     user.fullname = fullname;
+    // }
+
+    // if (email) {
+    //     user.email = email;
+    // }
+
+    const user = await User
+        .findByIdAndUpdate(req.user._id, { 
+            username, 
+            fullname,
+             email },
+             { new: true })
+        .select('-password -refresh_token');
+
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(200).json(new ApiResponse(200, null, 'Account details updated successfully'));
+});
+
+
+module.exports = {changePassword, getCurrentUser, updateAccountDetails};
