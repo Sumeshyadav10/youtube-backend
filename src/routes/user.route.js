2db446b7daac43dd84a959ec4ed2f0ce0060f4d0
Sumeshyadav10 <ysumesh084@gmail.com>
2025-02-22 23:23:01 +0530

    Add user account management routes for password change and details update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,7 @@ const upload = require('../middlewares/multer.middleware');
 const {loginUser,logoutUser} = require('../controllers/login.controller');
 const verifyJWT = require('../middlewares/auth.middleware');
 const { refreshToken } = require('../controllers/refreshToken.controller');
+const { changePassword, getCurrentUser, updateAccountDetails } = require('../controllers/changepassword');
 
 
 // router.post('/register',upload.fields
@@ -25,6 +26,9 @@ router.post('/register', registerUser);    // Route to register a user
 router.post('/login', loginUser);    // Route to login a user
 router.post('/logout',verifyJWT, logoutUser);    // Route to logout a user
 router.post('/refresh-token', refreshToken);    // Route to refresh the access token
+router.put('/change-password', verifyJWT, changePassword);    // Route to change the password
+router.get('/get-user', verifyJWT, getCurrentUser);    // Route to get the current user
+router.put('/update-account', verifyJWT, updateAccountDetails);    // Route to update the account details
 
 
 module.exports = router;    // Export the router
