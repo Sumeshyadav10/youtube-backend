9f7f872f378a02feca3222dedf041d5f0ec2fb56
Sumeshyadav10 <ysumesh084@gmail.com>
2025-02-27 22:28:28 +0530

    Add getWatchhistory controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -109,4 +109,55 @@ const AddProfileDetails = asynchandler(async (req, res) => {
    
 });
 
-module.exports = {registerUser, AddProfileDetails};
+const getWatchhistory = asynchandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'videos',
+                localField: 'watchHistory',
+                foreignField: '_id',
+                as: 'watchHistory',
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        username:1,
+                                        fullname:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                    }
+                }
+                ]
+
+            }
+            
+        },
+        {
+            $addFields : {
+                owner:{
+                    $first: "$owner"
+                }
+            }
+        },
+       
+    ]); 
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, 'watch History fetched successfully'));
+
+});
+
+module.exports = {registerUser, AddProfileDetails, getWatchhistory};
